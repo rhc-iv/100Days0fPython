80f2b7c5c50058ff42d60b0b2e0f93e9e91d9f68
Robert Carr <86175637+rhc-iv@users.noreply.github.com>
2022-12-25 07:29:54 -0500

    Uploaded 'main.py'

---
+++ Day 01/exercise-02-debugging-practice/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+#Fix the code below 👇
+
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#   print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
+
+# Added missing double-quote at beginning of string
+print("Day 1 - String Manipulation")
+
+# Replaced outer double-quotes with single-quotes
+print('String Concatenation is done with the "+" sign.')
+
+# Removed indention
+print('e.g. print("Hello " + "world")')
+
+# Removed extra "(" at beginning of function
+print("New lines can be created with a backslash and n.")
