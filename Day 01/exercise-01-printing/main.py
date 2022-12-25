46c83b9e13d76750cc5c04496f41128a6f7ed5e1
Robert Carr <86175637+rhc-iv@users.noreply.github.com>
2022-12-25 07:27:38 -0500

    Uploaded 'main.py' to directory

---
+++ Day 01/exercise-01-printing/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# See interactive exercise on codingrooms.com
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
