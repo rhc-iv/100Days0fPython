3fdfb94b235e1e02354fa3ebe4b86f2a2d6d8fa2
Robert Carr <86175637+rhc-iv@users.noreply.github.com>
2022-12-25 07:31:58 -0500

    Uploaded 'main.py'

---
+++ Day 01/exercise-03-input-function/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Using the input function, we can print information about the user's
+# input.  In this exercise, ask for the user's name, then print the
+# length of the user's name (expressed as a number).
+
+#Write your code below this line 👇
+
+print(len(input("What is your name?")))
