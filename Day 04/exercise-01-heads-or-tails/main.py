17c4c898c5d5abb23bef6c705890a79560cf5a6c
Robert Carr <86175637+rhc-iv@users.noreply.github.com>
2022-12-26 10:45:58 -0500

    Uploaded 'main.py'

--- Day 04/exercise-01-heads-or-tails/main.py
+++ Day 04/exercise-01-heads-or-tails/main.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3 
 
+# We need to import the random module before we begin
+# to write our code. 
+import random
+
+# Next, since this is a virtual coin-flip, we need to 
+# use the randint() function to calculate 2 random
+# numbers, in our case 0 or 1.
+result = random.randint(0, 1)
+
+# Next, we need to assign 1 to 'Heads' and 0 to 'Tails'
+# based on the random output, then print that output to
+# the console. We can use a simple 'if/else' statement
+# to accomplish this:
+if result == 1:
+    print("Heads")
+else:
+    print("Tails")
