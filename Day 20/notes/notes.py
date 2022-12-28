e244541bd620b6400ea505d4388d5f5655053338
Robert Carr <86175637+rhc-iv@users.noreply.github.com>
2022-12-28 11:50:37 -0500

    Uploaded .py file

---
+++ Day 20/notes/notes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# --- Create the classic Snake game using Python ---
+
+# First, let's break down the problem:
+
+#   1. Create a snake body
+#   2. Move the snake
+#   3. Control the snake
+#   4. Detect collision with food
+#   5. Create a scoreboard
+#   6. Detect collision with wall
+#   7. Detect collision with tail
+
