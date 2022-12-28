b7c8fd6fed9fbd270dc75e44271c391c02e4b53b
Robert Carr <86175637+rhc-iv@users.noreply.github.com>
2022-12-28 11:34:45 -0500

    Uploaded files

---
+++ Day 17/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+
+
+if quiz.still_has_questions() is False:
+    print("You've completed the quiz!")
+    print(f"Your final score is: {quiz.score}/{quiz.question_number}")
