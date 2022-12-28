cdd00e6e93253d2569922e32e868bfa96f1d383c
Robert Carr <86175637+rhc-iv@users.noreply.github.com>
2022-12-28 11:45:27 -0500

    Uploaded .py files

---
+++ Day 19/notes/event_listeners.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# ---- Event Listeners ----
+
+
+# Event listeners are functions within Python (and Python modules)
+# that "listen" for event triggered by users, such as key-press event
+# or mouse-click events.
+
---
+++ Day 19/notes/object_states_and_instances.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Object instances are simply objects created from the same class
+# that operate independently of one another. For example, 'tim',
+# which is our object based off of the Turtle() class can be iterated
+# as separate instances (or objects) as shown below:
+
+#   tim = Turtle()
+#   tom = Turtle()
+
+# Each of these objects, when called, will have their own attributes
+# & methods that will be acted upon separately within Python. The
+# attributes & methods of each of these objects are known as the
+# object's STATE.
