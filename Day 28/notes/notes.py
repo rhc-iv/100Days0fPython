512122e1ab59df1c9dcea3531e3a2b02f216ce9a
Robert Carr <86175637+rhc-iv@users.noreply.github.com>
2022-12-30 09:48:10 -0500

    Uploaded notes.py

---
+++ Day 28/notes/notes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# -> Dynamic Typing <-
+# --------------------
+
+# In Python, Dynamic Typing doesn't have anything to do with the act
+# of typing; rather, it is meant to be a reference to data types in
+# Python. Specifically, Dynamic Typing means that variable values can
+# be replaced with different data types & is valid. Unlike when we
+# try to perform certain operations between data types & are
+# presented with TypeErrors, Dynamic Typing functions perfectly in
+# Python.  For example:
+
+#   a = 3
+#   a = "Hello"
+#   print(a)
+
+# In the above example, we simply changed the data type of the
+# variable "a" from an integer to a string by simply declaring it.
