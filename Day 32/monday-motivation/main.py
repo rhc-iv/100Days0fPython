61445ebe553438d84d4a478c50a255fdb364b91a
Robert Carr <86175637+rhc-iv@users.noreply.github.com>
2022-12-31 13:39:14 -0500

    Add files via upload

--- Day 32/monday-motivation/main.py
+++ Day 32/monday-motivation/main.py
@@ -1,1 +1,32 @@
+#!/usr/bin/env python3
 
+# --- IMPORTS ---
+
+import datetime as dt
+import random
+import smtplib
+
+# --- CONSTANTS & GLOBAL VARIABLES ---
+
+MY_EMAIL = "august.hornet72@gmail.com"
+MY_PASSWORD = "dmufwdyiuopwyyxd"
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 0:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(
+            user=MY_EMAIL,
+            password=MY_PASSWORD,
+        )
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs="rhc.iv@icloud.com",
+            msg=f"Subject: Monday Motivation!\n\n{quote}".encode(
+                "utf-8"),
+        )
