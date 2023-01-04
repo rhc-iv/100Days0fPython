2c295afd89d3de50f202043739631e8a730905bc
Robert Carr <86175637+rhc-iv@users.noreply.github.com>
2023-01-04 14:28:43 -0500

    Add files via upload

--- Day 54/flask-web-dev/hello.py
+++ Day 54/flask-web-dev/hello.py
@@ -1,1 +1,23 @@
+#!/usr/bin/env python3
 
+# --- IMPORTS ---
+
+from flask import Flask
+
+# --- CONSTANTS & GLOBALS ---
+
+app = Flask(__name__)
+
+# --- METHODOLOGY ---
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/bye')
+def say_bye():
+    return 'Bye!'
+
+if __name__ == '__main__':
+    app.run()
